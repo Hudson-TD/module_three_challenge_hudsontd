c3972fe481092267d879257f72b74c31aae41684
Tyler Hudson <tylerhudson96@gmail.com>
2022-07-24 20:47:26 -0400

    final polish and comments

--- JS/script.js
+++ JS/script.js
@@ -6,47 +6,38 @@ var uppercaseChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'
 var numericChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialChars = ['!', '#', '$', '%', '&', '*', '?', '@', '+']
 
-// Get references to the #generate element
+// Get references to the #generate HTML id
 var generateBtn = document.querySelector("#generate");
 
-// Obtaining parameters for password creation
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Obtaining desired parameters for password creation
 function promptInputs() {
   userChoices = [];
   characterLength = parseInt(prompt("Please enter your desired password length (8-128 characters accepted"));
-
-// Checking that prompt reply is a numeric value
+// Checking that prompt reply is a numeric value & also fits the length contrains
   if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
     alert("Invalid entry! Please insert a number between 8 and 128");
     return false;
   }
+// Optional selections presented to user for password contents
   if (confirm("Do you want to include lowercase letters?")) {
     userChoices = userChoices.concat(lowercaseChars);
   }
   if (confirm("Do you want to include uppercase letters?")) {
     userChoices = userChoices.concat(uppercaseChars);
   }
-  if (confirm("Do you want to include special characters?")) {
-    userChoices = userChoices.concat(specialChars);
-  }
   if (confirm("Do you want to include numbers?")) {
     userChoices = userChoices.concat(numericChars);
   }
+  if (confirm("Do you want to include special characters?")) {
+    userChoices = userChoices.concat(specialChars);
+  }
   return true;
-
 }
 
-  // Write password to the #password input
-  function writePassword() {
-    var noIssues = promptInputs();
-
-    if (noIssues) {
-      var newPassword = generatePassword();
-      var passwordText = document.querySelector("#password");
-      passwordText.value = newPassword;
-    }
-  }
-
-//Creating password by grabbing values from array based on prompt choices
+//Creating password by grabbing random values from array with loop
 function generatePassword () {
   var password = "";
   for(var i = 0; i < characterLength; i++) {
@@ -56,5 +47,13 @@ function generatePassword () {
     return password;
   }
 
-  // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Writing generated password to the website
+function writePassword() {
+  var noIssues = promptInputs();
+
+  if (noIssues) {
+    var newPassword = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = newPassword;
+  }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+Description:
+
+The goal of this project was to create a functional password generator with some started code1. My approach was to create several arrays containing all of the possible characters that could comprise the password. Once the arrays were established I had to set up a series of window prompts that essentially return a true or false. Each input that is selected and returns true adds more characters to a seperate concatenated array that will be the character pool for the password. At the conclusion of the prompts a function taking advantage of a for loop and the built-in Math object puts a password together based on the chosen length. Lastly, and also instantaneously, the generated password takes advantage of the text area id in the HTML to write over the placeholder value with the random password that was generated.
+
+Live Website Link:
+
+
+
+Github Repository Link:
+
+
+
+Screenshot:
+
+[![Website-Screenshot.png](https://i.postimg.cc/7Lg4bRdH/Website-Screenshot.png)](https://postimg.cc/sBDqNT3q)
